1e37418e73160641084a8f56333f5967330249f2
leonardovalde <leonardovalde16@gmail.com>
2023-12-07 10:39:49 -0500

    astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://leonardovalde.github.io',
+  base: '/TeeiApp',
 })
